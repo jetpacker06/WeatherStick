790c29ae04a84745dd8ee2b302ec5bdf6ad7c79b
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2022-05-08 18:23:56 -0500

    merge files, logo

--- src/main/java/com/jetpacker06/weatherstick/WeatherStick.java
+++ src/main/java/com/jetpacker06/weatherstick/WeatherStick.java
@@ -1,9 +1,21 @@
 package com.jetpacker06.weatherstick;
 
+import net.minecraft.server.commands.WeatherCommand;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -17,4 +29,27 @@ public class WeatherStick {
         WeatherStickItem.register(eventBus);
         MinecraftForge.EVENT_BUS.register(this);
     }
+    public static class WeatherStickItem extends Item {
+        public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, WeatherStick.MOD_ID);
+        public static final RegistryObject<Item> WEATHER_STICK = ITEMS.register("weather_stick", () -> new WeatherStick.WeatherStickItem(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1)));
+        //end registering
+        public static void register(IEventBus eventBus) {
+            ITEMS.register(eventBus);
+        }
+        public WeatherStickItem(Properties pProperties) {
+            super(pProperties);
+        }
+
+        @Override
+        public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+            WeatherStick.LOGGER.info("Used");WeatherStick.LOGGER.info(pLevel.getClass());
+            if (pLevel instanceof ServerLevel) {
+                if (pLevel.getLevelData().isRaining()) {
+                    WeatherCommand.setClear(pPlayer.createCommandSourceStack().withLevel((ServerLevel) pLevel), 6000);
+                } else {
+                    WeatherCommand.setRain(pPlayer.createCommandSourceStack().withLevel((ServerLevel) pLevel), 6000);
+                }
+            }
+            return super.use(pLevel, pPlayer, pUsedHand);
+        }}
 }
