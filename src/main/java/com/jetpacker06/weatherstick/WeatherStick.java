66ca57da1a0f0af17e1d61900dd7766c84e3ff8e
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2023-06-15 17:29:20 -0500

    Update to 1.20

--- src/main/java/com/jetpacker06/weatherstick/WeatherStick.java
+++ src/main/java/com/jetpacker06/weatherstick/WeatherStick.java
@@ -1,23 +1,9 @@
 package com.jetpacker06.weatherstick;
 
-import net.minecraft.server.commands.WeatherCommand;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -29,12 +15,12 @@ public class WeatherStick {
     public WeatherStick() {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
         WeatherStickItem.register(eventBus);
-        eventBus.addListener(this::addToCreativeTabs);
+        //eventBus.addListener(this::addToCreativeTabs);
         MinecraftForge.EVENT_BUS.register(this);
     }
-    private void addToCreativeTabs(CreativeModeTabEvent.BuildContents event) {
-        if (event.getTab() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
-            event.accept(WeatherStickItem.WEATHER_STICK);
-        }
-    }
+   // private void addToCreativeTabs(CreativeModeTabRegistry) {
+   //     if (event.getTab() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+   //         event.accept(WeatherStickItem.WEATHER_STICK);
+   //     }
+   // }
 }
